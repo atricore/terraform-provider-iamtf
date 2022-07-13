7941a7bcc96e2c00fd698fb09694f2b1df9516c1
fbosch <felipebosch0@gmail.com>
2022-07-13 16:27:12 -0300

    idpfacebook completed

--- acctest-data/iamtf_idfacebook/idfacebook_updated.tf
+++ acctest-data/iamtf_idfacebook/idfacebook_updated.tf
@@ -6,15 +6,15 @@ resource "iamtf_identity_appliance" "test" {
 }
 
 resource "iamtf_idfacebook" "test" {
-  name        = "idFacebook-updated-replace_with_uuid"
-  element_id  = ""
-  description = "My IdFacebook updated"
-  location    = "http://localhost:8082"
-  client_id   = "my-client updated"
-  server_key  = "server-key for idpfacebook updated"
-  //Authorization endpoint
-  //Token endpoint
-  //Permissions
-  user_fields = "user-fields for idpfacebook updated"
-  ida         = iamtf_identity_appliance.test.name
+  name                 = "idFacebook-updated-replace_with_uuid"
+  element_id           = ""
+  description          = "My IdFacebook updated"
+  location             = "http://localhost:8082"
+  client_id            = "my-client updated"
+  server_key           = "server-key for idpfacebook updated"
+  authz_token_service  = "http://www.facebook.com:443/dialog/oauth"
+  access_token_service = "http://graph.facebook.com:443/oauth/access_token"
+  scopes               = "public_profile"
+  user_fields          = "gender"
+  ida                  = iamtf_identity_appliance.test.name
 }
