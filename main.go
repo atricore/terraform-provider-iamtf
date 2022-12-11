6a0acfaeaeaa7605fdb091fcf86e792917200977
Sebastian <sgonzalezoyuela@gmail.com>
2022-12-11 14:16:32 -0500

    Added -version flag

--- main.go
+++ main.go
@@ -3,20 +3,28 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/atricore/terraform-provider-iamtf/iamtf"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+var Version string
+
 func main() {
 
-	var debugMode bool
+	var debugMode, version bool
 
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.BoolVar(&version, "version", false, "set to true to displaoy provider version")
 	flag.Parse()
 
 	opts := &plugin.ServeOpts{ProviderFunc: iamtf.Provider}
+
+	if version {
+		fmt.Printf("pluginn version: %s\n", Version)
+	}
 
 	if debugMode {
 		// TODO: update this string with the full name of your provider as used in your configs
